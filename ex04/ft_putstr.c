4987a4bf3eae3ab74a4d49063d065a7025ec857f
Tim Chumbash <tchumbas@c1r11s3.42lisboa.com>
2025-08-06 13:13:29 +0100

    ex04

--- ex04/ft_putstr.c
+++ ex04/ft_putstr.c
@@ -6,13 +6,28 @@
 /*   By: tchumbas <tchumbas@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 12:41:57 by tchumbas          #+#    #+#             */
-/*   Updated: 2025/08/06 12:42:55 by tchumbas         ###   ########.fr       */
+/*   Updated: 2025/08/06 13:05:03 by tchumbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-void	ft_putstr(void){
-	
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+ 
+int	main(void)
+{
+	char *str;
+	str = "I'm a string LOL";
+	ft_putstr(str);
 }
